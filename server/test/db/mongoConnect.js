577e792681256bd87ab78cc38ef829fa654d5d43
Donovan <donovanvdl@Infoslips.com>
2020-05-12 14:39:43 +0200

    backup

--- server/test/db/mongoConnect.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-import { connectToMongo } from '../../db/mongoConnect';
-
-describe('Connect to mongo', function() {
-  it('Should return a db connction', function() {
-    console.log(connectToMongo)
-  });
-});
